a31107667dd35844d17313374f90ffb4ddc6e6c1
sujeong <tnwjd6496@naver.com>
2021-08-09 20:41:31 +0900

    '배달주소설정' 뷰 완료

--- Yogiyo_test_iOS_Flora/Yogiyo_test_iOS_Flora/Sources/View/Home/Location/Cell/CurrentLocationTVCell.swift
+++ Yogiyo_test_iOS_Flora/Yogiyo_test_iOS_Flora/Sources/View/Home/Location/Cell/CurrentLocationTVCell.swift
@@ -7,20 +7,28 @@
 
 import UIKit
 
+// 아무것도 없을 때 셀이랑 있을 때 셀 바꿔주기위해 셀 두개 만들거야
 class CurrentLocationTVCell: UITableViewCell {
     
+    @IBOutlet weak var cancelBtn: UIButton!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var subAddress: UILabel!
+    
     static let identifier = "CurrentLocationTVCell"
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "CurrentLocationTVCell", bundle: nil)
     }
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            // Initialization code
+        }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
 
         
     }
     
 }
+
--- Yogiyo_test_iOS_Flora/Yogiyo_test_iOS_Flora/Sources/View/Home/Location/CurrentLocationVC.swift
+++ Yogiyo_test_iOS_Flora/Yogiyo_test_iOS_Flora/Sources/View/Home/Location/CurrentLocationVC.swift
@@ -12,14 +12,24 @@ class CurrentLocationVC: UIViewController {
     @IBOutlet weak var locationTableView: UITableView!
     
     var address = [1,2,3]
+    var sections = ["최근주소"]
     
     @IBOutlet weak var backBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //locationTableView.delegate = self
-        //locationTableView.dataSource = self
+        locationTableView.delegate = self
+        locationTableView.dataSource = self
+        setStyle()
+        self.locationTableView.register(CurrentLocationTVCell.nib(), forCellReuseIdentifier: CurrentLocationTVCell.identifier)
+        
+        locationTableView.separatorColor = UIColor.clear
 
+    }
+    
+    func setStyle() {
+        // 테이블 뷰 경계션 없애기
+        locationTableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
     }
     
     @IBAction func backBtnTap(_ sender: Any) {
@@ -28,16 +38,59 @@ class CurrentLocationVC: UIViewController {
     
 }
 
-//extension CurrentLocationVC : UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return address.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
-//
-//
-//
-//
-//}
+extension CurrentLocationVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return address.count
+        }
+        else {
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CurrentLocationTVCell.identifier) as? CurrentLocationTVCell else{
+                return UITableViewCell()
+            }
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       return 80
+    }
+    
+    // 섹션
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+ 
+        if section == 0 {
+            let headerView = UIView()
+            headerView.backgroundColor = .white
+            headerView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50)
+            
+            let titleLabel = UILabel()
+            titleLabel.textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+            titleLabel.text = "최근 주소"
+            titleLabel.font = UIFont.systemFont(ofSize: 14, weight: .light)
+            titleLabel.frame = CGRect(x: 20, y: 10, width: headerView.frame.width, height: headerView.frame.height)
+            headerView.addSubview(titleLabel)
+            
+            return headerView
+        }
+
+        return UIView()
+    }
+
+}
